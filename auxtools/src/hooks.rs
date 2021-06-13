c4b526705046a872842a227772ae11a6e37cd285
Putnam <putnam3145@gmail.com>
2021-06-13 00:34:42 -0700

    oh, i REALLY hate it

--- auxtools/src/hooks.rs
+++ auxtools/src/hooks.rs
@@ -2,7 +2,7 @@ use super::proc::Proc;
 use super::raw_types;
 use super::value::Value;
 use crate::runtime::DMResult;
-use boomphf::{hashmap::BoomHashMap};
+use boomphf::Mphf;
 use detour::RawDetour;
 use std::ffi::c_void;
 use std::ffi::CStr;
@@ -94,36 +94,67 @@ pub struct ProcHook(pub fn(&Value, &Value, &mut Vec<Value>) -> DMResult);
 
 use std::fmt;
 
+use std::hash::Hash;
+
 impl fmt::Debug for ProcHook {
 	fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
 		write!(f, "function pointers with more than one ref argument don't actually work with debug, funny!")
 	}
 }
+// https://github.com/10XGenomics/rust-boomphf/blob/master/src/hashmap.rs#L22
+// this loop likes to go infinite
+struct BoomHashMapThatWorks<K: Clone + Hash + fmt::Debug + Send + Sync + std::cmp::PartialEq, V: Clone> {
+	mphf: Mphf<K>,
+	keys: Vec<K>,
+	values: Vec<V>,
+}
+
+impl<K: Clone + Hash + fmt::Debug + Send + Sync + std::cmp::PartialEq, V: Clone> BoomHashMapThatWorks<K, V> {
+	fn new_from_keys_and_pairs(keys: Vec<K>, mut pairs: Vec<(K, V)>) -> Self {
+		let mphf = Mphf::new_parallel(1.7, &keys, None);
+		pairs.sort_by_cached_key(|(k, _)| mphf.hash(&k));
+		BoomHashMapThatWorks {
+			mphf,
+			keys: pairs.iter().cloned().map(|(k, _)| k).collect(),
+			values: pairs.iter().cloned().map(|(_, v)| v).collect(),
+		}
+	}
+	pub fn new_from_pairs(pairs: Vec<(K, V)>) -> Self {
+		Self::new_from_keys_and_pairs(pairs.iter().cloned().map(|(k, _)| k).collect(), pairs)
+	}
+	pub fn get(&self, k: &K) -> Option<&V> {
+		self.mphf
+			.try_hash(k)
+			.and_then(|hash| {
+				let real_hash = hash as usize;
+				if self.keys.get(real_hash).map_or(false, |key| key == k) {
+					Some(real_hash)
+				} else {
+					None
+				}
+			})
+			.and_then(|hash| self.values.get(hash))
+	}
+}
 
 static mut SHOULD_GENERATE_HOOK_MAP: bool = false;
 
-static mut HOOKED_PROC_IDS: Option<Vec<raw_types::procs::ProcId>> = None;
-
-static mut HOOKED_PROC_VALUES: Option<Vec<ProcHook>> = None;
-
 // only the byond thread touches this so explicit thread local overhead is unnecessary
-static mut PROC_HOOKS: Option<BoomHashMap<raw_types::procs::ProcId, ProcHook>> = None;
+static mut PROC_HOOKS: Option<BoomHashMapThatWorks<raw_types::procs::ProcId, ProcHook>> = None;
+
+static mut PROC_HOOK_PAIRS: Option<Vec<(raw_types::procs::ProcId, ProcHook)>> = None;
 
 fn hook_by_id(id: raw_types::procs::ProcId, hook: ProcHook) -> Result<(), HookFailure> {
 	unsafe {
-		if HOOKED_PROC_IDS.is_none() {
-			HOOKED_PROC_IDS = Some(Vec::new());
+		if PROC_HOOK_PAIRS.is_none() {
+			PROC_HOOK_PAIRS = Some(Vec::new());
 		}
-		if HOOKED_PROC_VALUES.is_none() {
-			HOOKED_PROC_VALUES = Some(Vec::new());
-		}
-		for &extant_id in HOOKED_PROC_IDS.as_ref().unwrap().iter() {
+		for &(extant_id, _) in PROC_HOOK_PAIRS.as_ref().unwrap().iter() {
 			if extant_id == id {
 				return Err(HookFailure::AlreadyHooked);
 			}
 		}
-		HOOKED_PROC_IDS.as_mut().unwrap().push(id);
-		HOOKED_PROC_VALUES.as_mut().unwrap().push(hook);
+		PROC_HOOK_PAIRS.as_mut().unwrap().push((id, hook));
 		if SHOULD_GENERATE_HOOK_MAP {
 			generate_hook_map();
 		}
@@ -134,8 +165,7 @@ fn hook_by_id(id: raw_types::procs::ProcId, hook: ProcHook) -> Result<(), HookFa
 pub fn clear_hooks() {
 	unsafe {
 		PROC_HOOKS = None;
-		HOOKED_PROC_IDS = None;
-		HOOKED_PROC_VALUES = None;
+		PROC_HOOK_PAIRS = None;
 		SHOULD_GENERATE_HOOK_MAP = false;
 	}
 }
@@ -149,9 +179,8 @@ pub fn hook<S: Into<String>>(name: S, hook: ProcHook) -> Result<(), HookFailure>
 
 pub fn generate_hook_map() {
 	unsafe {
-		PROC_HOOKS = Some(BoomHashMap::new_parallel(
-			HOOKED_PROC_IDS.as_ref().unwrap().clone(),
-			HOOKED_PROC_VALUES.as_ref().unwrap().clone(),
+		PROC_HOOKS = Some(BoomHashMapThatWorks::new_from_pairs(
+			PROC_HOOK_PAIRS.as_ref().unwrap().clone(),
 		));
 		SHOULD_GENERATE_HOOK_MAP = true;
 	}
